a979c586ee049a17da37fa8457f71326134b5113
mennamohamed0207 <menamohamed0207@gmail.com>
2024-09-12 20:16:01 +0300

    adding packet class

---
+++ Packet.cpp
@@ -0,0 +1,74 @@
+#include <Packet.h>
+
+Packet::Packet(string destAddress, string srcAddress, string ethernetType, string payload, string CRC, string IFG, string SOP, string preamble)
+{
+    this->preamble = preamble;
+    this->SOP = SOP;
+    this->destAddress = destAddress;
+    this->srcAddress = srcAddress;
+    this->ethernetType = ethernetType;
+    this->payload = payload;
+    this->CRC = CRC;
+    this->IFG = IFG;
+}
+
+string Packet::getSOP()
+{
+    return SOP;
+}
+string Packet::getDestAddress()
+{
+    return destAddress;
+}
+string Packet::getSrcAddress()
+{
+    return srcAddress;
+}
+string Packet::getEthernetType()
+{
+    return ethernetType;
+}
+string Packet::getPayload()
+{
+    return payload;
+}
+string Packet::getCRC()
+{
+    return CRC;
+}
+string Packet::getIFG()
+{
+    return IFG;
+}
+void Packet::setPreamble(string preamble)
+{
+    this->preamble = preamble;
+}
+void Packet::setSOP(string SOP)
+{
+    this->SOP = SOP;
+}
+void Packet::setDestAddress(string destAddress)
+{
+    this->destAddress = destAddress;
+}
+void Packet::setSrcAddress(string srcAddress)
+{
+    this->srcAddress = srcAddress;
+}
+void Packet::setEthernetType(string ethernetType)
+{
+    this->ethernetType = ethernetType;
+}
+void Packet::setPayload(string payload)
+{
+    this->payload = payload;
+}
+void Packet::setCRC(string CRC)
+{
+    this->CRC = CRC;
+}
+void Packet::setIFG(string IFG)
+{
+    this->IFG = IFG;
+}
--- Packet.h
+++ Packet.h
@@ -1,5 +1,34 @@
-class Packet{
+#include <iostream>
+using namespace std;
+class Packet
+{
 private:
+    string preamble;
+    string SOP;
+    string destAddress;
+    string srcAddress;
+    string ethernetType;
+    string payload;
+    string CRC;
+    string IFG;
+
 
 public:
+    Packet(string destAddress, string srcAddress, string ethernetType, string payload, string CRC, string IFG,string SOP="D5",string preamble="FB555555555555");
+    string getPreamble();
+    string getSOP();
+    string getDestAddress();
+    string getSrcAddress();
+    string getEthernetType();
+    string getPayload();
+    string getCRC();
+    string getIFG();
+    void setPreamble(string preamble);
+    void setSOP(string SOP);
+    void setDestAddress(string destAddress);
+    void setSrcAddress(string srcAddress);
+    void setEthernetType(string ethernetType);
+    void setPayload(string payload);
+    void setCRC(string CRC);
+    void setIFG(string IFG);
 };
