5f447ebfef026684eaed04f78e8e88ee0c112be3
mennamohamed0207 <menamohamed0207@gmail.com>
2024-09-12 21:02:42 +0300

    fixing errors

--- Program.cpp
+++ Program.cpp
@@ -1,11 +1,15 @@
-#include "program.h"
 #include <iostream>
 #include <fstream>
-#include "configurations.cpp" // Include the configurations header
+#include <string>
+#include <configurations.h>
+using namespace std;
+#include <Program.h>
 
-void Program::setConfigurationFile(const std::string& configurationFile) {
-    // Check if the file exists and throw an exception if it doesn't
-    if (!fileExists(configurationFile)) {
+void Program::setConfigurationFile(const std::string &configurationFile)
+{
+
+    if (!fileExists(configurationFile))
+    {
         throw std::runtime_error("File not found");
     }
 
@@ -13,34 +17,39 @@ void Program::setConfigurationFile(const std::string& configurationFile) {
     readConfig();
 }
 
-void Program::setOutputFile(const std::string& outputFile) {
+void Program::setOutputFile(const std::string &outputFile)
+{
     this->outputFile = outputFile;
 }
 
-void Program::setPackets(const std::string& packets) {
+void Program::setPackets(const std::string &packets)
+{
     this->packets = packets;
 }
 
-void Program::readConfig() {
-    // Read the configuration file using the configurations class
-    config.readConfigurations(configurationFile);
+void Program::readConfig()
+{
+
+    this->config.readConfigurations(configurationFile);
 }
 
-void Program::writeFile() {
-    // Open the output file for writing
+void Program::writeFile()
+{
+
     std::ofstream file(outputFile);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         std::cerr << "Error: Could not open the file " << outputFile << std::endl;
         return;
     }
 
-    // Write the packets to the file
     file << packets;
 
     file.close();
 }
 
-bool Program::fileExists(const std::string& fileName) {
+bool Program::fileExists(const std::string &fileName)
+{
     std::ifstream file(fileName);
     return file.is_open();
 }
--- program.h
+++ program.h
@@ -1,18 +1,21 @@
 
 
 #include <string>
+#include <configurations.h>
 
-class Program {
+class Program
+{
 public:
-    void setConfigurationFile(const std::string& configurationFile);
-    void setOutputFile(const std::string& outputFile);
-    void setPackets(const std::string& packets);
+    void setConfigurationFile(const std::string &configurationFile);
+    void setOutputFile(const std::string &outputFile);
+    void setPackets(const std::string &packets);
     void readConfig();
     void writeFile();
+    bool fileExists(const std::string &fileName);
 
 private:
     std::string configurationFile;
     std::string outputFile;
     std::string packets;
+    configurations config;
 };
-
