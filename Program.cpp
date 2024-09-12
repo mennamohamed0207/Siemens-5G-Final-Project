f1aec451e61eb8900379cd6c189e2a78b6ab183c
mennamohamed0207 <menamohamed0207@gmail.com>
2024-09-12 19:12:19 +0300

    adding validation on file read and write

--- Program.cpp
+++ Program.cpp
@@ -10,6 +10,7 @@ private:
     string outputFile;
     string packets;
 
+public:
     void readFile()
     {
         std::ifstream file(configurationFile);
@@ -27,18 +28,59 @@ private:
 
         file.close();
     }
-    
+
     void writeFile()
     {
-        std::ofstream file(outputFile); 
+        std::ofstream file(outputFile);
         if (!file.is_open())
-        { 
+        {
             std::cerr << "Error: Could not open the file " << outputFile << std::endl;
             return;
         }
 
-        file << packets; 
+        file << packets;
 
-        file.close(); 
+        file.close();
+    }
+
+    bool fileExists(const std::string &fileName)
+    {
+        std::ifstream file(fileName);
+        return file.is_open();
+    }
+    void setConfigurationFile(string configurationFile)
+    {
+        if (fileExists(configurationFile))
+        {
+            this->configurationFile = configurationFile;
+        }
+        else
+        {
+            throw std::runtime_error("File not found");
+        }
+    }
+    void setOutputFile(string outputFile)
+    {
+
+        this->outputFile = outputFile;
+    }
+    void setPackets(string packets)
+    {
+        this->packets = packets;
     }
 };
+int main()
+{
+    Program program;
+    try{
+    program.setConfigurationFile("config.txt");
+    program.setOutputFile("output.txt");
+    program.setPackets("packets.txt");
+    program.readFile();
+    program.writeFile();
+    return 0;
+
+    }catch(const std::exception& e){
+        std::cerr << e.what() << '\n';
+    }
+}
